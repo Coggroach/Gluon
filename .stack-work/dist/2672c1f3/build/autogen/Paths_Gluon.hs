c4ce464c1bbed1da33433968d2f1738b14af5426
Coggroach <xpgear2@gmail.com>
2016-12-09 02:36:43 +0000

    Added Servant FileServer

---
+++ .stack-work/dist/2672c1f3/build/autogen/Paths_Gluon.hs
@@ -0,0 +1,35 @@
+module Paths_Gluon (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "E:\\Development\\Haskell\\Projects\\Gluon\\.stack-work\\install\\09641122\\bin"
+libdir     = "E:\\Development\\Haskell\\Projects\\Gluon\\.stack-work\\install\\09641122\\lib\\x86_64-windows-ghc-7.10.3\\Gluon-0.1.0.0-IVcQzZwl5VwDXZ5KQ484Tt"
+datadir    = "E:\\Development\\Haskell\\Projects\\Gluon\\.stack-work\\install\\09641122\\share\\x86_64-windows-ghc-7.10.3\\Gluon-0.1.0.0"
+libexecdir = "E:\\Development\\Haskell\\Projects\\Gluon\\.stack-work\\install\\09641122\\libexec"
+sysconfdir = "E:\\Development\\Haskell\\Projects\\Gluon\\.stack-work\\install\\09641122\\etc"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "Gluon_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "Gluon_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "Gluon_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "Gluon_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "Gluon_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
---
+++ src/FileServer.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase    #-}
+{-# LANGUAGE TypeOperators #-}
+
+module FileServer where
+
+import           Control.Monad.Trans.Except
+import           Control.Monad.IO.Class
+import           Data.Aeson
+import           GHC.Generics
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           System.IO
+import           System.Directory
+
+data FileServer = FileServer { address :: String, port :: Int, name :: String }
+
+resources :: String
+resources = "res/FileServers";
+
+type FileApi = 
+    "files" :> Get '[JSON] [String] :<|>
+    "download" :> Capture "fileName" String :> Get '[JSON] File :<|>
+    "upload" :> ReqBody '[JSON] File :> Post '[JSON] File
+
+type Handler = ExceptT ServantErr IO
+
+data File = File { fileName :: FilePath, fileContent :: String } deriving (Eq, Show, Generic)
+
+instance ToJSON File
+instance FromJSON File
+
+fileApi :: Proxy FileApi
+fileApi = Proxy
+
+server :: Server FileApi
+server = 
+    getFiles :<|>
+    downloadFile :<|>
+    uploadFile
+
+fileApp :: Application
+fileApp = serve fileApi server
+
+mkApp :: IO()
+mkApp = run 8081 fileApp 
+
+getFiles :: Handler [FilePath]
+getFiles = liftIO (getDirectoryContents resources)
+
+downloadFile :: FilePath -> Handler File
+downloadFile f = do    
+    content <- liftIO (readFile (resources ++ "/" ++ f))
+    return (File f content)
+
+uploadFile :: File -> Handler File
+uploadFile (File f c) = do    
+    liftIO (writeFile (resources ++ "/" ++ f) c)
+    return (File f c)
+
+
+
