b4c7b3b3dfcf1e981405e47bbf22ae62fef1b7a5
Coggroach <xpgear2@gmail.com>
2016-12-09 22:08:13 +0000

    Added CommonServerApi

---
+++ src/CommonServerApi.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase    #-}
+{-# LANGUAGE TypeOperators #-}
+
+module CommonServerApi where
+
+import           Control.Monad.Trans.Except
+import           Data.Aeson
+import           Network.Wai
+import           Servant
+import           CommonServer
+
+type ApiHandler = ExceptT ServantErr IO
+
+type FileApi = 
+    "files" :> Get '[JSON] [FilePath] :<|>
+    "download" :> Capture "fileName" String :> Get '[JSON] CommonServer.File :<|>
+    "upload" :> ReqBody '[JSON] CommonServer.File :> Post '[JSON] CommonServer.Response
+
+type DirectoryApi = 
+    "files" :> Get '[JSON] [String] :<|>
+    "files" :> Capture "uuid" String :> Get '[JSON] [String] :<|>
+    "open" :> Capture "fileName" String :> Get '[JSON] CommonServer.File :<|>
+    "close" :> ReqBody '[JSON] CommonServer.File :> Post '[JSON] CommonServer.Response
+
+type HandshakeApi =
+  "shake" :> ReqBody '[JSON] CommonServer.Identity :> Post '[JSON] CommonServer.Response :<|>
+  "echo" :> Capture "param" String :> Get '[JSON] String
+
